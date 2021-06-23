cb9e0b551be99ceadd9cb8e88c02fc97d111f534
Naomi <n.valdovinos74@gmail.com>
2021-06-22 21:14:46 -0400

    trying to get console table to work

--- server.js
+++ server.js
@@ -1,64 +1,75 @@
-const inquirer = require('inquirer');
-const mysql = require('mysql');
-// const table = require('console.table');
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const cTable = require("console.table");
 
 const connection = mysql.createConnection({
-    hot: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Rm55ilyuh@',
-    database: 'employee_trackerDB'
+  hot: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Rm55ilyuh@",
+  database: "employee_trackerDB",
 });
 
 connection.connect((err) => {
-    if (err) throw err;
-    tracker();
+  if (err) throw err;
+  tracker();
 });
 
 const tracker = () => {
-    inquirer.prompt({
-        type: 'list',
-        name: 'task',
-        message: 'What would you like to do? ',
-        choices: [
-            'Add Employee',
-            'View Employee',
-            'Add Department',
-            'View Departmanet',
-            'View Role',
-            'Update Employee Role',
-            'QUIT'
-        ]
-    }).then((answer) => {
-        switch (answer.action) {
-            case 'Add Employee: ':
-                addEmplpoyee();
-                // console.log('this works');
-                break;
-            case 'View Employee':
-                vieeEmployee();
-                break;
-            case 'Add Department':
-                addDepartment();
-                break;
+  inquirer
+    .prompt({
+      type: "list",
+      name: "task",
+      message: "What would you like to do? ",
+      choices: [
+        "Add Employee",
+        "View Employee",
+        "Add Department",
+        "View Departmanet",
+        "View Role",
+        "Update Employee Role",
+        "QUIT",
+      ],
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case "Add Employee: ":
+          addEmplpoyee();
+          // console.log('this works');
+          break;
+        case "View Employee":
+          viewEmployee();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
 
-            case 'View Department':
-                viewDepartment();
-                break;
+        case "View Department":
+          viewDepartment();
+          break;
 
-            case 'View role':
-                viewRole();
-                break;
+        case "View role":
+          viewRole();
+          break;
 
-            case 'Update Employee Role':
-                updateEmployee();
-                break;
+        case "Update Employee Role":
+          updateEmployee();
+          break;
 
-            case 'QUIT':
-                connectiom.end();
-                break;
-        }
+        case "QUIT":
+          connectiom.end();
+          break;
+      }
     });
 };
 
 
+//will try to get this to work once im done
+const viewEmployee = () => {
+  connection.query('SELECT * FROM employee', (err, res) => {
+    if (err) throw err;
+    console.log("View Employee");
+    console.table(res);
+    tracker();
+  });
+};
